0e11800299c55df8826fc94d8b34cfb5eb059d9f
JN Veigel <art.jnveigel@gmail.com>
2018-11-12 15:26:39 +0100

    initial version, done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router  = express.Router();
 
+const Movie = require("../models/Movie.js")
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+	Movie.find()
+		.then(movieResult => {
+			res.locals.movieList = movieResult;
+			res.render("movies")
+		})
+		.catch(err => next(err));
+});
+
+router.get("/movie/:movieId", (req, res, next) => {
+	const { movieId } = req.params;
+
+	Movie.findById(movieId)
+		.then(selectMovie => {
+			res.render("movie-info", {selectMovie})
+		})
+})
+
+
 
 module.exports = router;
